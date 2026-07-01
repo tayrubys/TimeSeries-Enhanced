78dbf140b35af62853d651cb994838a561f22593
Berkay Çelik <celikberkay004@gmail.com>
2026-07-01 16:28:14 +0300

    otomata için PCA adımı eklendi

--- src/data_pipeline/preprocessing/balance_adasyn.py
+++ src/data_pipeline/preprocessing/balance_adasyn.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from imblearn.over_sampling import ADASYN
+from sklearn.decomposition import PCA
 
 X_train = pd.read_csv('data/processed/batadal_X_train_scaled.csv')
 y_train = pd.read_csv('data/processed/batadal_y_train.csv')
@@ -29,3 +30,20 @@ X_train_adasyn_df.to_csv('data/processed/batadal_X_train_adasyn.csv', index=Fals
 y_train_adasyn_df.to_csv('data/processed/batadal_y_train_adasyn.csv', index=False)
 
 print("Dengelenmiş veriler 'data/processed/' klasörüne kaydedildi.\n")
+
+print("Otomata için PCA:")
+pca = PCA(n_components=1, random_state=42)
+X_train_adasyn_pc1 = pca.fit_transform(X_train_adasyn)
+
+X_train_adasyn_df = pd.DataFrame(X_train_adasyn, columns=X_train.columns)
+y_train_adasyn_df = pd.DataFrame(y_train_adasyn, columns=y_train.columns)
+
+X_train_adasyn_pc1_df = pd.DataFrame(X_train_adasyn_pc1, columns=['PC1'])
+
+X_train_adasyn_df.to_csv('data/processed/batadal_X_train_adasyn.csv', index=False)
+y_train_adasyn_df.to_csv('data/processed/batadal_y_train_adasyn.csv', index=False)
+X_train_adasyn_pc1_df.to_csv('data/processed/batadal_X_train_adasyn_pc1.csv', index=False)
+
+print("=" * 50)
+print("Standart ADASYN ve Otomata için ADASYN_PC1 dosyaları kaydedildi")
+print("=" * 50 + "\n")
